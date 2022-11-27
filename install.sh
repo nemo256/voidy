2db5871222bc0e45bc438d167d0e3308ea8a4901
Amine Neggazi <neggazimedlamine@gmail.com>
2022-11-27 18:46:45 +0100

    added parted!

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Set a bold custom terminus font
-xbps-install -Syu terminus-font gptfdisk cfdisk
+xbps-install -Syu terminus-font gptfdisk parted
 # setfont ter-v32b
 
 ################################################
