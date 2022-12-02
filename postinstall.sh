2ac44d3ad22a3fdad8009abd90c5bc307f859d9b
Amine Neggazi <neggazimedlamine@gmail.com>
2022-12-02 19:40:34 +0100

    testing on vm!

--- postinstall.sh
+++ postinstall.sh
@@ -29,14 +29,14 @@ echo -ne "
                                 Setup Language And Locale
 ------------------------------------------------------------------------------------------
 "
-sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
-locale-gen
-timedatectl --no-ask-password set-timezone ${TIMEZONE}
-timedatectl --no-ask-password set-ntp 1
-localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_TIME="en_US.UTF-8"
-ln -s /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
+# sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+# locale-gen
+# timedatectl --no-ask-password set-timezone ${TIMEZONE}
+# timedatectl --no-ask-password set-ntp 1
+# localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_TIME="en_US.UTF-8"
+# ln -s /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
 # Set keymaps
-localectl --no-ask-password set-keymap ${KEYMAP}
+# localectl --no-ask-password set-keymap ${KEYMAP}
 
 # Add sudo no password rights
 # sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
